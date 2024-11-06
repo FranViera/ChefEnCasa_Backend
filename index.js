03f4f45ec1a17ab4990b4265952d8dab97310549
FranViera <129343029+FranViera@users.noreply.github.com>
2024-11-06 15:17:38 -0300

    Update index.js
    Se agrega lista vacia a lista de compras

--- index.js
+++ index.js
@@ -1072,7 +1072,7 @@ app.post('/preparar-receta-spoonacular', authenticateToken, async (req, res) => 
   }
 });
 
-//VER LISTA DE COMPRAS
+// VER LISTA DE COMPRAS
 app.get('/lista-de-compras', authenticateToken, async (req, res) => {
   try {
     const db = await connectToDatabase();
@@ -1080,10 +1080,12 @@ app.get('/lista-de-compras', authenticateToken, async (req, res) => {
     const listaDeCompras = await db.collection('listasDeCompras').findOne({ usuarioId, completada: false });
 
     if (!listaDeCompras) {
-      return res.status(404).json({ message: 'No tienes lista de compras' });
+      // Enviar una señal específica para el frontend cuando no haya lista
+      return res.status(200).json({ message: 'No tienes ningún ingrediente en tu lista de compras', listaVacia: true });
     }
 
-    res.status(200).json(listaDeCompras);
+    // Responder con la lista de compras si existe
+    res.status(200).json({ ...listaDeCompras, listaVacia: false });
   } catch (error) {
     res.status(500).json({ error: `Error al obtener la lista de compras: ${error.message}` });
   }
