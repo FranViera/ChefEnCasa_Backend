bb1e6c4bfe7ce8550affc9ed73e4099f770322a0
FranViera <129343029+FranViera@users.noreply.github.com>
2024-12-02 17:46:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3027,19 +3027,25 @@ async function obtenerSabiasQue() {
 
 app.get('/sabiasque', async (req, res) => {
   try {
-    console.log('Consultando datos de "¿Sabías que?"');
-    const data = await obtenerSabiasQue();
+    const db = await connectToDatabase();
+    const data = await db.collection('sabiasQue').find({}).toArray();
+
     if (!data || data.length === 0) {
-      console.log('No se encontraron datos.');
       return res.status(404).json({ error: 'No se encontraron datos' });
     }
-    console.log('Datos obtenidos:', data);
-    res.status(200).json(data);
+
+    // Agrega la URL completa a las imágenes
+    const baseUrl = 'https://chefencasabackend-production.up.railway.app';
+    const datosConImagenes = data.map(item => ({
+      ...item,
+      imagen: `${baseUrl}/${item.imagen}` // Construye la URL completa
+    }));
+
+    res.status(200).json(datosConImagenes);
   } catch (error) {
-    console.error('Error en /sabiasque:', error);
+    console.error('Error en /sabiasque:', error.message);
     res.status(500).json({ error: 'Error interno del servidor' });
   }
 });
-
 
 
